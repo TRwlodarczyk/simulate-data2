dc7a9a62befa4f929debdcba08bdc66fd204ed92
Tomasz <twlodarczyk@arizona.edu>
2023-02-14 10:34:58 -0700

    Add simulation and plot script

---
+++ simulate-plot-data2.R
@@ -0,0 +1,21 @@
+# Simulate and plot data
+# Tomasz Wlodarczyk
+# twlodarczyk@arizona.edu
+# 2023-02-14
+
+# Simulate predictor variable
+
+x <- rnorm(n=100)
+
+
+# Simulate response variable with some noise
+
+y <- 2*x + rnorm(n =100, sd=0.2)
+
+# Plot the data
+
+plot(x=x, y=y)
+
+plot(density(x))
+plot(density(y))
+#shapiro.test(x)
